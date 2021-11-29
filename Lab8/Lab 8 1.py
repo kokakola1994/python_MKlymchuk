72c3e378f3a0737de8a1c5539cd2b805fad94301
Mykola Klymchuk <m.klymchuk@student.po.edu.pl>
2021-11-29 14:35:36 +0100

    Lab 8 Exer 1, 2

---
+++ Lab8/Lab 8 1.py
@@ -0,0 +1,3 @@
+fo = open("p.txt","w")
+fo.write('Hello world!\nMy name is Mykola Klymchuk\nProgramed in Python languge')
+fo.close()
---
+++ Lab8/Lab 8 2.py
@@ -0,0 +1,10 @@
+fo = open("p.txt", "r")
+while 1:
+     
+    s = fo.read(3)        
+    if s == "":
+        break
+         
+    print(s)
+ 
+fo.close()
